4c87ced9ca3b2fea5d747b2a3d699e0e4e8e772f
Vats-tech <kumarvats417@gmail.com>
2025-05-07 00:46:31 +0530

    fix pre merge

--- src/components/common/confirmation-modal.tsx
+++ src/components/common/confirmation-modal.tsx
@@ -6,11 +6,7 @@ interface ConfirmationModalType {
   onCancel: () => void;
 }
 
-const ConfirmationModal = ({
-  label,
-  onConfirm,
-  onCancel,
-}: ConfirmationModalType) => {
+const ConfirmationModal = ({ onConfirm, onCancel }: ConfirmationModalType) => {
   return (
     <div className="modal-overlay">
       <div className="absolute left-1/2 top-1/2 transform-translate-x-1/2-translate-y-1/2 text-sm shadow-lg z-50 w-[420px] p-6 rounded-md backdrop-blur-lg bg-[#ffff]">
